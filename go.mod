fa80299fa6b60f03f4e768c501bedda00ae99318
Xiezx63 <fontainebleau729@gmail.com>
2026-08-03 10:41:35 +0800

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Xiezx63/mytools
+
+go 1.26.5
---
+++ proxy.go
@@ -0,0 +1,33 @@
+package mytools 
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// Proxy 根据 open 状态返回对应的 http.Client
+func Proxy(open bool, proxyattr string) *http.Client {
+	// 不开代理直接返回默认 Client，避免 else 嵌套
+	if !open {
+		return &http.Client{}
+	}
+
+	//默认地址URL
+	URL := "http://127.0.0.1:10808"
+	if len(proxyattr)!=0{
+		URL = proxyattr
+	}
+
+	proxyURL, err := url.Parse(URL)
+	if err != nil {
+		fmt.Println("代理地址解析失败:", err)
+		return &http.Client{} // 发生解析错误时给一个保底的默认 Client，防止调用方拿到 nil 报空指针异常
+	}
+
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+}
